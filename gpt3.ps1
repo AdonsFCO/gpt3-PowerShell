a99b440fa6433c4b893efa671ab607e4e688c90a
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2023-01-14 10:14:02 -0400

    [REF] Correct a mistake on the comments.

--- gpt3.ps1
+++ gpt3.ps1
@@ -2,8 +2,8 @@
 $env:OPENAI_API_KEY = "API-KEY"
 
 # Set the model to use (e.g. "text-davinci-003")
-/////////$MODEL = "text-davinci-003"///////////
-///////////////////////////////////////////////
+$MODEL = "text-davinci-003"
+
 $PROMPT = $args[0]
 $TOKEN_COUNT = 300
 $TEMPERATURE = 0.4
